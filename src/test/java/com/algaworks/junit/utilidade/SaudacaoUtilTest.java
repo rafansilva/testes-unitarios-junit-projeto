180ec09d86e619e18ee042bb6a5f315bdc82a1b5
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2026-07-24 11:28:48 -0300

    3.1. Organizando testes com o padrão Triple A

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -8,8 +8,13 @@ class SaudacaoUtilTest {
 
     @Test
     public void saudarComBomDia() {
+        //Arrange - prepara o cenário
         int hora = 9;
+
+        //Act - executa o cenário
         String saudacao = SaudacaoUtil.saudar(hora);
+
+        //Assert - realiza a asserção
         assertEquals("Bom dia", saudacao);
     }
 
@@ -30,30 +35,27 @@ class SaudacaoUtilTest {
     @Test
     public void saudarComBomDiaAPartir5h() {
         String saudacao = SaudacaoUtil.saudar(5);
-
         assertEquals("Bom dia", saudacao, "Saudação incorreta!");
-
     }
 
     @Test
     public void saudarComBoaNoiteAte4h() {
         String saudacao = SaudacaoUtil.saudar(4);
-
         assertEquals("Boa noite", saudacao, "Saudação incorreta!");
-
     }
 
 
     @Test
     public void deveLancarException() {
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> SaudacaoUtil.saudar(-10));
-
-        assertEquals("Hora inválida", illegalArgumentException.getMessage());
+        int horaInvalida = -10;
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> SaudacaoUtil.saudar(horaInvalida));
+        assertEquals("Hora inválida", exception.getMessage());
     }
 
     //pode não ser util nesse contexto, mas existe essa possibilidade
     @Test
     public void naoDeveLancarException() {
-        assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
+        int horaValida = 0;
+        assertDoesNotThrow(() -> SaudacaoUtil.saudar(horaValida));
     }
 }
